bb58fd62a91fe5937fc35636c0d3e0731131d557
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-24 15:28:32 +0200

    Feat: add array and print in page

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTEL</title>
+</head>
+<body>  
+
+        <?php foreach ($hotels as $hotel): ?>
+            <ul>
+                <li><?php echo ($hotel['name']); ?></li>
+                <li><?php echo ($hotel['description']); ?></li>
+                <li><?php echo $hotel['parking'] ? 'Sì' : 'No'; ?></li>
+                <li><?php echo ($hotel['vote']); ?></li>
+                <li><?php echo ($hotel['distance_to_center']); ?> km</li>
+            </ul>
+        <?php endforeach; ?>
+
+</body>
+</html>
